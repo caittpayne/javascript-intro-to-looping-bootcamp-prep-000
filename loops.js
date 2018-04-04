091c6a36cec872619fc3e519d0cf88794db9f6b6
Cait Payne <caitpayne@outlook.com>
2018-04-04 00:22:33 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,20 @@
+function forLoop(array) {
+  for(let i = 0; i < 25; i++) {
+    if(i === 1) {
+     array.push("I am 1 strange loops.")
+    }
+    else {
+    array.push("I am ${i} strange loops.");
+    }
+  }
+  
+  return array; 
+}
+
+function whileLoop(int) {
+  while (int > 0) {
+    console.log(--int)
+  }
+  return('done')
+}
+
